ff1e51a9171c80b0bb8e821ce1e591c663864c5f
shahzainfarooqi <142692117+shahzainfarooqi@users.noreply.github.com>
2025-04-27 18:16:47 +0500

    Add files via upload

---
+++ square.py
@@ -0,0 +1,8 @@
+# Asking the user for a number
+number = float(input("Type a number to see its square: "))
+
+# Calculating the square
+square = number * number
+
+# Printing the result
+print(str(number) + " squared is " + str(square))
